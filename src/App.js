ca56c8979614f79a08849ff61359f5dfb082cb33
oscarporras02 <oscarrussi@outlook.com>
2021-03-30 17:50:33 -0500

    add most of the content to project componenet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import './App.css';
+import { nanoid } from 'nanoid';
+import useState from 'react-usestateref';
 import Footsection from './components/Footsection';
 import Navigation from './components/Navigation';
 import Presentation from './components/Presentation';
 import Project from './components/Project';
 
 function App() {
+  const projects = [{
+    name: 'Bogotá Houses', description: 'Directory of real states in Bogotá for selling and renting. Pure HTML and CSS project.', github: 'https://github.com/andresporras3423/bogota-houses', live: 'http://andresporres.000webhostapp.com/bogota-houses/main.html', url: './src/assets/bogota-houses.png', tools: ['HTML/CSS', 'Bootstrap'],
+  },
+  {
+    name: 'Weather Bot', description: 'This telegram bot give you updates of temperature of any city or country in the world.', github: 'https://github.com/andresporras3423/weather-bot-ruby', live: 'https://web.telegram.org/#/im?p=@weather3423_bot', url: './src/assets/weather-bot.png', tools: ['Ruby', 'Telegram API'],
+  },
+  {
+    name: 'The Cool Writer', description: 'A blog with articles about lot of different random topics. Any user can sign up and add articles', github: 'https://github.com/andresporras3423/articles-project', live: 'https://blueberry-custard-86240.herokuapp.com/', url: './src/assets/articles.png', tools: ['Ruby On Rails', 'Ruby', 'SQL', 'HTML/CSS', 'Bootstrap'],
+  },
+  {
+    name: 'English Project', description: 'A website with diferente resources and features to improve your english', github: 'https://github.com/andresporras3423/english-project', live: 'https://englishproject000.000webhostapp.com/ep/index.html', url: './src/assets/english-project.png', tools: ['Vue.js', 'JavaScript', 'SQL', 'PHP', 'HTML/CSS', 'Bootstrap'],
+  },
+  {
+    name: 'Platform game', description: 'A  platform game built with Phaser3 and JavaScript. Inspired by the running dinosaur game', github: 'https://github.com/andresporras3423/platform-game', live: 'https://gifted-poitras-77933b.netlify.app/dist/index.html', url: './src/assets/platform-game.png', tools: ['JavaScript', 'HTML/CSS', 'Webpack'],
+  }];
+
+  const filterProjects = () => projects;
   return (
     <div className="App">
       <div>
         <Navigation />
         <main>
           <Presentation />
-          <Project />
+          {
+      filterProjects().map((cProject, index) => (
+        <Project project={cProject} index={index} key={nanoid()} />
+      ))
+  }
         </main>
         <Footsection />
         Hello world
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,54 @@
-function Project() {
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+
+function Project(props) {
+  const { project, index } = props;
+  console.log(`current project is: ${JSON.stringify(project)}`);
+  const photo = 'photo';
+  const row1 = 'row1';
+  const direction = ['g-column1', 'g-column2'];
+  const giveUrl = () => {
+    const style = { backgroundImage: `url('${project.url}')` };
+    return style;
+  };
+  const linkCV = (url) => {
+    window.open(url, '_blank');
+  };
   return (
-    <div />
+    <section className="project-properties shadow-style">
+      <div className={[direction[index % 2], photo, row1].join(' ')} style={giveUrl()} />
+      <div className="[direction[(index+1)%2], info, row2]">
+        <h3 className="project-title">{project.name}</h3>
+        <p className="project-description">{project.description}</p>
+        {
+                    project.tools.map((tool) => (
+                      <span className="tools-style" key={nanoid()}>{tool}</span>
+                    ))
+                  }
+        <div>
+          <button type="submit" className="btn btn-primary button-link-style" onClick={() => linkCV(project.github)}>GitHub</button>
+          <button type="submit" className="btn btn-primary button-link-style" onClick={() => linkCV(project.live)}>Live</button>
+        </div>
+      </div>
+    </section>
   );
 }
+
+Project.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    github: PropTypes.string,
+    live: PropTypes.string,
+    url: PropTypes.string,
+    tools: PropTypes.instanceOf(Array),
+  }),
+  index: PropTypes.number,
+};
+
+Project.defaultProps = {
+  project: null,
+  index: 0,
+};
 
 export default Project;
