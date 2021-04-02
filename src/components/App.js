842e1791d69f92a41bb65201ea4f3502f40ddbea
oscarporras02 <oscarrussi@outlook.com>
2021-04-02 17:41:55 -0500

    using redux in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,14 @@
 import '../App.css';
 import { nanoid } from 'nanoid';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Footsection from './Footsection';
 import Navigation from './Navigation';
 import Presentation from './Presentation';
 import Project from './Project';
 
-function App() {
-  const projects = [{
-    name: 'Bogotá Houses', description: 'Directory of real states in Bogotá for selling and renting. Pure HTML and CSS project.', github: 'https://github.com/andresporras3423/bogota-houses', live: 'http://andresporres.000webhostapp.com/bogota-houses/main.html', url: './src/assets/bogota-houses.png', tools: ['HTML/CSS', 'Bootstrap'],
-  },
-  {
-    name: 'Weather Bot', description: 'This telegram bot give you updates of temperature of any city or country in the world.', github: 'https://github.com/andresporras3423/weather-bot-ruby', live: 'https://web.telegram.org/#/im?p=@weather3423_bot', url: './src/assets/weather-bot.png', tools: ['Ruby', 'Telegram API'],
-  },
-  {
-    name: 'The Cool Writer', description: 'A blog with articles about lot of different random topics. Any user can sign up and add articles', github: 'https://github.com/andresporras3423/articles-project', live: 'https://blueberry-custard-86240.herokuapp.com/', url: './src/assets/articles.png', tools: ['Ruby On Rails', 'Ruby', 'SQL', 'HTML/CSS', 'Bootstrap'],
-  },
-  {
-    name: 'English Project', description: 'A website with diferente resources and features to improve your english', github: 'https://github.com/andresporras3423/english-project', live: 'https://englishproject000.000webhostapp.com/ep/index.html', url: './src/assets/english-project.png', tools: ['Vue.js', 'JavaScript', 'SQL', 'PHP', 'HTML/CSS', 'Bootstrap'],
-  },
-  {
-    name: 'Platform game', description: 'A  platform game built with Phaser3 and JavaScript. Inspired by the running dinosaur game', github: 'https://github.com/andresporras3423/platform-game', live: 'https://gifted-poitras-77933b.netlify.app/dist/index.html', url: './src/assets/platform-game.png', tools: ['JavaScript', 'HTML/CSS', 'Webpack'],
-  },
-  {
-    name: 'Pokemon catalogue', description: 'A pokedex with the top 100 pokemons. It Uses redux to the state managements, and routes for every pokemon details.', github: 'https://github.com/andresporras3423/catalogue-react', live: 'https://oscar-russi-pokedex.netlify.app/', url: './src/assets/pokemon-catalogue.png', tools: ['JavaScript', 'React'],
-  }];
-
+function App(props) {
+  const { projects } = props;
   const filterProjects = () => projects;
   return (
     <div id="app">
@@ -46,4 +30,22 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  projects: state.projectData.projects,
+});
+
+App.propTypes = {
+  projects: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    github: PropTypes.string.isRequired,
+    tools: PropTypes.instanceOf(Array).isRequired,
+  }),
+};
+
+App.defaultProps = {
+  projects: null,
+};
+
+export default connect(mapStateToProps)(App);
