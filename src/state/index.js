8ee1e6865b727d79dd79c118d3ecd17a8d677115
oscarporras02 <oscarrussi@outlook.com>
2021-05-09 09:46:45 -0500

    add count-letter project to my portfolio

--- src/state/index.js
+++ src/state/index.js
@@ -1,22 +1,28 @@
 const initialState = {
-  projects: [{
-    name: 'Bogotá Houses', description: 'Directory of real states in Bogotá for selling and renting. Pure HTML and CSS project.', github: 'https://github.com/andresporras3423/bogota-houses', live: 'http://andresporres.000webhostapp.com/bogota-houses/main.html', url: './src/assets/bogota-houses.png', tools: ['HTML/CSS', 'Bootstrap'],
-  },
+  projects: [
+    {
+      name: 'Count letters', description: 'The name is self descriptive, count how many of the specified letter you can see in the matrix..', github: 'https://github.com/andresporras3423/count-letters-front', live: 'https://count-letters-andres-porras.netlify.app/', url: './src/assets/count-letters.png', tools: ['JavaScript', 'React', 'HTML/CSS', 'Bootstrap', 'Ruby', 'Ruby on Rails', 'SQL'],
+    },
+    {
+      name: 'Pokemon catalogue', description: 'A pokedex with the top 100 pokemons. It Uses redux to the state managements, and routes for every pokemon details.', github: 'https://github.com/andresporras3423/catalogue-react', live: 'https://oscar-russi-pokedex.netlify.app/', url: './src/assets/pokemon-catalogue.png', tools: ['JavaScript', 'React', 'HTML/CSS', 'Bootstrap'],
+    },
+    ,
   {
-    name: 'Weather Bot', description: 'This telegram bot give you updates of temperature of any city or country in the world.', github: 'https://github.com/andresporras3423/weather-bot-ruby', live: 'https://web.telegram.org/#/im?p=@weather3423_bot', url: './src/assets/weather-bot.png', tools: ['Ruby', 'Telegram API'],
+    name: 'Platform game', description: 'A  platform game built with Phaser3 and JavaScript. Inspired by the running dinosaur game', github: 'https://github.com/andresporras3423/platform-game', live: 'https://gifted-poitras-77933b.netlify.app/dist/index.html', url: './src/assets/platform-game.png', tools: ['JavaScript', 'HTML/CSS', 'Webpack'],
   },
   {
     name: 'The Cool Writer', description: 'A blog with articles about lot of different random topics. Any user can sign up and add articles', github: 'https://github.com/andresporras3423/articles-project', live: 'https://blueberry-custard-86240.herokuapp.com/', url: './src/assets/articles.png', tools: ['Ruby On Rails', 'Ruby', 'SQL', 'HTML/CSS', 'Bootstrap'],
   },
   {
+    name: 'Weather Bot', description: 'This telegram bot give you updates of temperature of any city or country in the world.', github: 'https://github.com/andresporras3423/weather-bot-ruby', live: 'https://web.telegram.org/#/im?p=@weather3423_bot', url: './src/assets/weather-bot.png', tools: ['Ruby', 'Telegram API'],
+  },
+  {
+    name: 'Bogotá Houses', description: 'Directory of real states in Bogotá for selling and renting. Pure HTML and CSS project.', github: 'https://github.com/andresporras3423/bogota-houses', live: 'http://andresporres.000webhostapp.com/bogota-houses/main.html', url: './src/assets/bogota-houses.png', tools: ['HTML/CSS', 'Bootstrap'],
+  },
+  {
     name: 'English Project', description: 'A website with diferente resources and features to improve your english', github: 'https://github.com/andresporras3423/english-project', live: 'https://englishproject000.000webhostapp.com/ep/index.html', url: './src/assets/english-project.png', tools: ['Vue.js', 'JavaScript', 'SQL', 'PHP', 'HTML/CSS', 'Bootstrap'],
-  },
-  {
-    name: 'Platform game', description: 'A  platform game built with Phaser3 and JavaScript. Inspired by the running dinosaur game', github: 'https://github.com/andresporras3423/platform-game', live: 'https://gifted-poitras-77933b.netlify.app/dist/index.html', url: './src/assets/platform-game.png', tools: ['JavaScript', 'HTML/CSS', 'Webpack'],
-  },
-  {
-    name: 'Pokemon catalogue', description: 'A pokedex with the top 100 pokemons. It Uses redux to the state managements, and routes for every pokemon details.', github: 'https://github.com/andresporras3423/catalogue-react', live: 'https://oscar-russi-pokedex.netlify.app/', url: './src/assets/pokemon-catalogue.png', tools: ['JavaScript', 'React', 'HTML/CSS', 'Bootstrap'],
-  }],
+  }
+],
 };
 
 export default initialState;
