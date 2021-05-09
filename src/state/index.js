c9bcdc5716099e52de99bf5bc27b4cc1d77aa3e3
oscarporras02 <oscarrussi@outlook.com>
2021-05-09 09:57:16 -0500

    update margin and padding for the filter tool

--- src/state/index.js
+++ src/state/index.js
@@ -1,7 +1,7 @@
 const initialState = {
   projects: [
     {
-      name: 'Count letters', description: 'The name is self descriptive, count how many of the specified letter you can see in the matrix..', github: 'https://github.com/andresporras3423/count-letters-front', live: 'https://count-letters-andres-porras.netlify.app/', url: './src/assets/count-letters.png', tools: ['JavaScript', 'React', 'HTML/CSS', 'Bootstrap', 'Ruby', 'Ruby on Rails', 'SQL'],
+      name: 'Count letters', description: 'The name of this game is self descriptive, count how many of the specified letter you can see in the matrix.', github: 'https://github.com/andresporras3423/count-letters-front', live: 'https://count-letters-andres-porras.netlify.app/', url: './src/assets/count-letters.png', tools: ['JavaScript', 'React', 'HTML/CSS', 'Bootstrap', 'Ruby', 'Ruby on Rails', 'SQL'],
     },
     {
       name: 'Pokemon catalogue', description: 'A pokedex with the top 100 pokemons. It Uses redux to the state managements, and routes for every pokemon details.', github: 'https://github.com/andresporras3423/catalogue-react', live: 'https://oscar-russi-pokedex.netlify.app/', url: './src/assets/pokemon-catalogue.png', tools: ['JavaScript', 'React', 'HTML/CSS', 'Bootstrap'],
